3f96ffcc9a69abcdf9e1868af7909e85912e83d7
johnaxel24 <146420352+johnaxel24@users.noreply.github.com>
2025-01-22 20:29:49 +0800

    updated TASConstraints class

--- cicss/src/main/java/com/example/cicss/model/TASConstraint.java
+++ cicss/src/main/java/com/example/cicss/model/TASConstraint.java
@@ -3,8 +3,12 @@ package com.example.cicss.model;
 import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.Type;
+import org.hibernate.type.SqlTypes;
 
 import java.util.List;
+import java.util.Map;
 
 
 @Entity
@@ -17,21 +21,23 @@ public class TASConstraint {
     private String tasStatus;
     @ElementCollection
     private List<String> specialtyCourses;
+//    @ElementCollection
+//    private List<String> dayRestriction;
+//    @ElementCollection
+//    private List<String> timeRestriction;
     @ElementCollection
-    private List<String> dayRestriction;
-    @ElementCollection
-    private List<String> timeRestriction;
+    @JdbcTypeCode(SqlTypes.JSON)
+    private Map<String, TimeBlock> dayAndTimeRestriction;
 
     public TASConstraint() {
     }
 
-    public TASConstraint(String tasConstraintId, String tasName, String tasStatus, List<String> specialtyCourses, List<String> dayRestriction, List<String> timeRestriction) {
+    public TASConstraint(String tasConstraintId, String tasName, String tasStatus, List<String> specialtyCourses, Map<String, TimeBlock> dayAndTimeRestriction) {
         this.tasConstraintId = tasConstraintId;
         this.tasName = tasName;
         this.tasStatus = tasStatus;
         this.specialtyCourses = specialtyCourses;
-        this.dayRestriction = dayRestriction;
-        this.timeRestriction = timeRestriction;
+        this.dayAndTimeRestriction = dayAndTimeRestriction;
     }
 
     public String getTasConstraintId() {
@@ -66,19 +72,69 @@ public class TASConstraint {
         this.specialtyCourses = specialtyCourses;
     }
 
-    public List<String> getDayRestriction() {
-        return dayRestriction;
+    public Map<String, TimeBlock> getDayAndTimeRestriction() {
+        return dayAndTimeRestriction;
     }
 
-    public void setDayRestriction(List<String> dayRestriction) {
-        this.dayRestriction = dayRestriction;
+    public void setDayAndTimeRestriction(Map<String, TimeBlock> dayAndTimeRestriction) {
+        this.dayAndTimeRestriction = dayAndTimeRestriction;
     }
 
-    public List<String> getTimeRestriction() {
-        return timeRestriction;
-    }
-
-    public void setTimeRestriction(List<String> timeRestriction) {
-        this.timeRestriction = timeRestriction;
-    }
+    //
+//    public TASConstraint(String tasConstraintId, String tasName, String tasStatus, List<String> specialtyCourses, List<String> dayRestriction, List<String> timeRestriction) {
+//        this.tasConstraintId = tasConstraintId;
+//        this.tasName = tasName;
+//        this.tasStatus = tasStatus;
+//        this.specialtyCourses = specialtyCourses;
+//        this.dayRestriction = dayRestriction;
+//        this.timeRestriction = timeRestriction;
+//    }
+//
+//    public String getTasConstraintId() {
+//        return tasConstraintId;
+//    }
+//
+//    public void setTasConstraintId(String tasConstraintId) {
+//        this.tasConstraintId = tasConstraintId;
+//    }
+//
+//    public String getTasName() {
+//        return tasName;
+//    }
+//
+//    public void setTasName(String tasName) {
+//        this.tasName = tasName;
+//    }
+//
+//    public String getTasStatus() {
+//        return tasStatus;
+//    }
+//
+//    public void setTasStatus(String tasStatus) {
+//        this.tasStatus = tasStatus;
+//    }
+//
+//    public List<String> getSpecialtyCourses() {
+//        return specialtyCourses;
+//    }
+//
+//    public void setSpecialtyCourses(List<String> specialtyCourses) {
+//        this.specialtyCourses = specialtyCourses;
+//    }
+//
+//    public List<String> getDayRestriction() {
+//        return dayRestriction;
+//    }
+//
+//    public void setDayRestriction(List<String> dayRestriction) {
+//        this.dayRestriction = dayRestriction;
+//    }
+//
+//    public List<String> getTimeRestriction() {
+//        return timeRestriction;
+//    }
+//
+//    public void setTimeRestriction(List<String> timeRestriction) {
+//        this.timeRestriction = timeRestriction;
+//    }
 }
