1a67e88c1512b8dda635676e21ef30f1604dab8d
johnaxel24 <146420352+johnaxel24@users.noreply.github.com>
2025-03-02 00:15:47 +0800

    TASConstraint_Controller updated

--- src/main/java/ust/com/cicss/dao/TASRepository.java
+++ src/main/java/ust/com/cicss/dao/TASRepository.java
@@ -1,3 +1,4 @@
+
 package ust.com.cicss.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -6,11 +7,21 @@ import org.springframework.stereotype.Repository;
 
 import ust.com.cicss.models.TAS;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Repository
 public interface TASRepository extends JpaRepository<TAS, String> {
     @Query(
-        value = "SELECT COUNT(*) > 0 FROM teaching_academic_staff WHERE email = ?1",
-        nativeQuery = true
+            value = "SELECT COUNT(*) > 0 FROM teaching_academic_staff WHERE email = ?1",
+            nativeQuery = true
     )
     boolean authByEmail(String email);
+
+    @Query(value = "SELECT tas_id FROM backend.teaching_academic_staff", nativeQuery = true)
+    List<String> getAllTASIds();
+
+    @Query(value = "SELECT name, units, courses, restrictions FROM teaching_academic_staff WHERE tas_id = ?", nativeQuery = true)
+    Object getTASConstraintById(String TASId);
+
 }
