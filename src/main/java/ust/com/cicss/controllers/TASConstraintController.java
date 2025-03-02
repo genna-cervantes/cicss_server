7d6101ff89814c5e1c368b107c3613fb1bce45f5
johnaxel24 <146420352+johnaxel24@users.noreply.github.com>
2025-03-02 20:27:07 +0800

    updated TASConstraint controller

--- src/main/java/ust/com/cicss/controllers/TASConstraintController.java
+++ src/main/java/ust/com/cicss/controllers/TASConstraintController.java
@@ -5,6 +5,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import ust.com.cicss.dao.TASRepository;
+import ust.com.cicss.models.Restrictions;
 import ust.com.cicss.models.TAS;
 import ust.com.cicss.models.TASConstraint;
 import ust.com.cicss.services.TASService;
@@ -63,24 +65,57 @@ public class TASConstraintController {
         repo.save(tas);
     }
 
-    @PutMapping
-    public void updateTASConstraint(@RequestBody TAS tas)
+    @PutMapping("")
+    public void updateTASConstraint(@RequestBody Map<String, Object> updates)
     {
         //repo.save(tasConstraint);
         // ang structure ng request body should be tas_id tapos kung ano lng iuupdate like
         // REQ BODY: {tas_id: 'PF12345678', name: 'Edited Name', units: 24} -- name and units lng ung ieedit
 
         // UPDATE teaching_academic_staff updatecolumn = updatedcolvalue WHERE tas_id = tas_id
-        repo.save(tas);
+        boolean firstEntry = true;
+        String tas_id = "";
+        String column = "";
+        Object value = null;
+
+        for (Map.Entry<String, Object> entry : updates.entrySet()) {
+            if (firstEntry) {
+                tas_id = entry.getValue().toString(); // First entry is tas_id
+                firstEntry = false;
+            } else {
+                column = entry.getKey(); // Next key as column
+                value = entry.getValue(); // Next value
+                System.out.println("tas_id: " + tas_id + ", column: " + column + ", value: " + value);
+
+                switch (column) {
+                    case "name":
+                        repo.updateName(tas_id, (String) value);
+                        break;
+                    case "units":
+                        repo.updateUnits(tas_id, (int) value);
+                        break;
+                    case "email":
+                        repo.updateEmail(tas_id, (String) value);
+                        break;
+                    case "courses":
+                        repo.updateCourses(tas_id, (String[]) value);
+                    case "restrictions":
+                        repo.updateRestrictions(tas_id, (Restrictions) value);
+                    default:
+                        throw new IllegalArgumentException("Invalid column name: " + column);
+                }
+            }
+        }
     }
 
     @DeleteMapping
-    public void deleteTASConstraint(@RequestBody TAS tas)
+    public void deleteTASConstraint(@RequestBody Map<String, String> tas_id)
     {
         //repo.delete(tasConstraint);
         // REQ BODY: {tas_id: 'PF12345678'}
-
+        Map.Entry<String, String> entry = tas_id.entrySet().iterator().next();
+        String value = entry.getValue();
         // UPDATE teaching_academic_staff is_active = 1 WHERE tas_id = tas_id
-        repo.delete(tas);
+        repo.deleteById(value);
     }
 }
--- src/main/java/ust/com/cicss/dao/TASRepository.java
+++ src/main/java/ust/com/cicss/dao/TASRepository.java
@@ -1,10 +1,13 @@
 
 package ust.com.cicss.dao;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import ust.com.cicss.models.Restrictions;
 import ust.com.cicss.models.TAS;
 
 import java.util.ArrayList;
@@ -23,5 +26,30 @@ public interface TASRepository extends JpaRepository<TAS, String> {
 
     @Query(value = "SELECT name, units, courses, restrictions FROM teaching_academic_staff WHERE tas_id = ?", nativeQuery = true)
     Object getTASConstraintById(String TASId);
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE backend.teaching_academic_staff SET name = ?2 WHERE tas_id = ?1", nativeQuery = true)
+    void updateName(String tas_id, String name);
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE backend.teaching_academic_staff SET units = ?2 WHERE tas_id = ?1", nativeQuery = true)
+    void updateUnits(String tas_id, int units);
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE backend.teaching_academic_staff SET email = ?2 WHERE tas_id = ?1", nativeQuery = true)
+    void updateEmail(String tas_id, String email);
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE backend.teaching_academic_staff SET courses = ?2 WHERE tas_id = ?1", nativeQuery = true)
+    void updateCourses(String tas_id, String[] courses);
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE backend.teaching_academic_staff SET restrictions = ?2 WHERE tas_id = ?1", nativeQuery = true)
+    void updateRestrictions(String tas_id, Restrictions restrictions);
 
 }
